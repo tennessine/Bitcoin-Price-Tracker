df63f65b06dc6a40ee5148f65f8d3eee98d78589
Tennessine <389443626@qq.com>
2019-09-04 20:59:21 +0800

    first commit

--- Bitcoin Price Tracker/ViewController.swift
+++ Bitcoin Price Tracker/ViewController.swift
@@ -10,11 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usdLabel: UILabel!
+    
+    @IBOutlet weak var chineseLabel: UILabel!
+    
+    @IBOutlet weak var euLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        getDefaultPrices()
+        getPrice()
     }
-
-
+    
+    func getDefaultPrices() {
+        let usdPrice = UserDefaults.standard.double(forKey: "USD")
+        if usdPrice != 0.0 {
+            usdLabel.text = doubleToMoneyString(price: usdPrice, currencyCode: "USD")
+        }
+        
+        let cnyPrice = UserDefaults.standard.double(forKey: "CNY")
+        if cnyPrice != 0.0 {
+            chineseLabel.text = doubleToMoneyString(price: cnyPrice, currencyCode: "CNY")
+        }
+        
+        let euPrice = UserDefaults.standard.double(forKey: "EUR")
+        if euPrice != 0.0 {
+            euLabel.text = doubleToMoneyString(price: euPrice, currencyCode: "EUR")
+        }
+    }
+    
+    
+    @IBAction func refreshTapped(_ sender: Any) {
+        getPrice()
+    }
+    
+    func doubleToMoneyString(price: Double, currencyCode: String) -> String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = currencyCode
+        
+        return formatter.string(from: NSNumber(value: price))
+    }
+    
+    func getPrice() {
+        if let url = URL(string: "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,CNY,EUR") {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Double] {
+                        
+                        DispatchQueue.main.async {
+                            if let usdPrice = json["USD"] {
+                                
+                                self.usdLabel.text = self.doubleToMoneyString(price: usdPrice, currencyCode: "USD")
+                                
+                                UserDefaults.standard.set(usdPrice, forKey: "USD")
+                            }
+                            
+                            if let chinesePrice = json["CNY"] {
+                                self.chineseLabel.text = self.doubleToMoneyString(price: chinesePrice, currencyCode: "CNY")
+                                
+                                UserDefaults.standard.set(chinesePrice, forKey: "CNY")
+                            }
+                            
+                            if let euPrice = json["EUR"] {
+                                self.euLabel.text = self.doubleToMoneyString(price: euPrice, currencyCode: "EUR")
+                                
+                                UserDefaults.standard.set(euPrice, forKey: "EUR")
+                            }
+                            
+                            UserDefaults.standard.synchronize()
+                        }
+                    }
+                } else {
+                    print("something went wrong")
+                }
+            }.resume()
+        }
+    }
 }
 
